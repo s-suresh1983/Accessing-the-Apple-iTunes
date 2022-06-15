15554f9ad1b97536aa564e2a3dcf174bee4159d3
suresh babu <46187428+s-suresh1983@users.noreply.github.com>
2022-06-15 12:30:06 +0530

    commit2

--- code.py
+++ code.py
@@ -9,8 +9,8 @@ URL = "https://itunes.apple.com/search?term=" + term + "&entity=album"
 res = requests.get(URL)
 data = res.json()
 
-srch_count = int(data['resultCount'])
-print("The search returned", srch_count, "results.")
+search_count = int(data['resultCount'])
+print("The search returned", search_count, "results.")
 
 for row in data['results']:
     print("Artist:", row['artistName'], "- Album:", row['collectionName'], "- Track Count:", row['trackCount'])
